79f1cfea9255526659bbec9a136bbda925f85502
Ryan Styron <ryanstyron1220@gmail.com>
2020-04-11 13:27:31 -0400

    Rewrite Heal.java.

--- src/mc/rysty/heliosphereplugin/commands/Heal.java
+++ src/mc/rysty/heliosphereplugin/commands/Heal.java
@@ -4,7 +4,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 import mc.rysty.heliosphereplugin.HelioSpherePlugin;
@@ -12,46 +11,38 @@ import mc.rysty.heliosphereplugin.utils.MessageUtils;
 
 public class Heal implements CommandExecutor {
 
-	HelioSpherePlugin plugin = HelioSpherePlugin.getInstance();
-	FileConfiguration config = plugin.getConfig();
-
 	public Heal(HelioSpherePlugin plugin) {
 		plugin.getCommand("heal").setExecutor(this);
 	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (sender.hasPermission("hs.heal")) {
-			if (args.length == 0) {
-				if (!(sender instanceof Player)) {
-					sender.sendMessage(MessageUtils.chat(config.getString("console_player_error_message")));
-					return true;
-				}
-				Player p = (Player) sender;
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (command.getName().equalsIgnoreCase("heal")) {
+			if (sender.hasPermission("hs.heal")) {
+				Player target = null;
 
-				p.setHealth(20);
-				p.setFoodLevel(20);
-				p.sendMessage(MessageUtils.chat(config.getString("HealCommand.heal")));
-				return true;
-			} else if (args.length == 1) {
-				Player t = Bukkit.getPlayer(args[0]);
+				if (args.length > 0)
+					target = Bukkit.getPlayer(args[0]);
+				else if (sender instanceof Player)
+					target = (Player) sender;
 
-				if (t == null) {
-					sender.sendMessage(MessageUtils.chat(config.getString("player_offline_message")));
+				if (target == null)
+					MessageUtils.validPlayerError(sender);
+				else {
+					String displayName = target.getDisplayName();
+					@SuppressWarnings("all")
+					double maxHealth = target.getMaxHealth();
+
+					target.setHealth(maxHealth);
+					target.setFoodLevel(20);
+
+					if (target != sender)
+						MessageUtils.configStringMessage(sender, "HealCommand.heal_other", "<target>", displayName);
+					MessageUtils.configStringMessage(target, "HealCommand.heal");
 				}
-				t.setHealth(20);
-				t.setFoodLevel(20);
-				sender.sendMessage(MessageUtils.chat(
-						plugin.getConfig().getString("HealCommand.heal_other").replaceAll("<target>", t.getName())));
-				return true;
-			} else if (args.length > 1) {
-				sender.sendMessage(MessageUtils.chat(config.getString("too_many_args_error")));
-				return false;
-			}
-		} else {
-			sender.sendMessage(MessageUtils.chat(config.getString("no_perm_message")));
+			} else
+				MessageUtils.noPermissionError(sender);
 		}
 		return false;
 	}
-
 }

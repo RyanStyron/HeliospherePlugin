d0da6968989dd2dcfadeb8cee679442ea5dd1b74
Ryan Styron <ryanstyron1220@gmail.com>
2020-04-18 10:34:05 -0400

    Update InvseeCommand.java.

--- src/mc/rysty/heliosphereplugin/commands/inventory/InvseeCommand.java
+++ src/mc/rysty/heliosphereplugin/commands/inventory/InvseeCommand.java
@@ -4,58 +4,58 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.Inventory;
 
 import mc.rysty.heliosphereplugin.HelioSpherePlugin;
 import mc.rysty.heliosphereplugin.utils.MessageUtils;
 
 public class InvseeCommand implements CommandExecutor {
 
-	HelioSpherePlugin plugin = HelioSpherePlugin.getInstance();
-	FileConfiguration config = plugin.getConfig();
-
 	public InvseeCommand(HelioSpherePlugin plugin) {
 		plugin.getCommand("invsee").setExecutor(this);
 	}
 
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("invsee")) {
-			if (!(sender instanceof Player)) {
-				sender.sendMessage(MessageUtils.chat(config.getString("console_error_message")));
-				return false;
-			}
-			Player p = (Player) sender;
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (command.getName().equalsIgnoreCase("invsee")) {
+			if (sender.hasPermission("hs.staff")) {
+				if (sender instanceof Player) {
+					if (args.length < 2) {
+						Player player = (Player) sender;
+						Player target = null;
 
-			if (p.hasPermission("hs.invsee")) {
-				if (args.length == 1) {
-					Player t = Bukkit.getPlayer(args[0]);
+						if (args.length == 1)
+							target = Bukkit.getPlayer(args[0]);
+						else
+							target = player;
 
-					if (t == null) {
-						p.sendMessage(MessageUtils.chat(config.getString("player_offline_message")));
-					} else {
-						PlayerInventory inv = t.getInventory();
-						String tName = t.getDisplayName();
+						if (target == null)
+							MessageUtils.validPlayerError(sender);
+						else {
+							Inventory targetInventory;
+							targetInventory = Bukkit.createInventory(target, 45, target.getName() + "'s Inventory");
+							targetInventory.setContents(target.getInventory().getContents());
+							targetInventory.setContents(target.getInventory().getArmorContents());
 
-						p.sendMessage(MessageUtils.chat(config.getString("InvSeeCommand.inv_opened_message").replaceAll("<player>", tName)));
-						if ((!p.hasPermission("hs.invsee.modify") || (t.hasPermission("hs.invsee.preventmodify")))) {
-							//p.sendMessage(ChatColor.RED + "You do not have permission to edit " + tName +  ChatColor.RED +  "'s inventory");
-							p.openInventory(inv);
-						} else if ((p.hasPermission("hs.invsee.modify")) && (!t.hasPermission("hs.invsee.preventmodify"))) {
-							p.openInventory(inv);
-						} 
-					}
-				} else {
-					p.sendMessage(MessageUtils.chat(config.getString("InvSeeCommand.argument_error")));
-					return false;
-				}
-			} else {
-				sender.sendMessage(MessageUtils.chat(config.getString("no_perm_message")));
-			}
+							if (target.hasPermission("hs.admin") && !player.hasPermission("hs.admin")) {
+								player.openInventory(targetInventory);
+							} else
+								player.openInventory(targetInventory);
+
+							if (target != player)
+								MessageUtils.configStringMessage(sender, "InvSeeCommand.invsee-other-message",
+										"<player>", target.getDisplayName());
+							else
+								MessageUtils.configStringMessage(sender, "InvSeeCommand.invsee-message");
+						}
+					} else
+						MessageUtils.configStringMessage(sender, "InvSeeCommand.argument-error");
+				} else
+					MessageUtils.consoleError();
+			} else
+				MessageUtils.noPermissionError(sender);
 		}
 		return false;
 	}
-
 }

d54501e381e55fff9e105b9c4e9f935473906456
RyanStyron <58631871+RyanStyron@users.noreply.github.com>
2021-01-30 17:58:16 -0500

    Update CustomChatFormat.java.

--- src/mc/rysty/heliosphereplugin/chat/CustomChatFormat.java
+++ src/mc/rysty/heliosphereplugin/chat/CustomChatFormat.java
@@ -10,20 +10,14 @@ import mc.rysty.heliosphereplugin.utils.MessageUtils;
 
 public class CustomChatFormat implements Listener {
 
-	HelioSpherePlugin plugin = HelioSpherePlugin.getInstance();
-	FileConfiguration config = plugin.getConfig();
+	private FileConfiguration config = HelioSpherePlugin.getInstance().getConfig();
 
 	@EventHandler
 	public void onChat(AsyncPlayerChatEvent event) {
 		String playerDisplayName = "%s";
 		String message = "%s";
-		String customChatFormat = convertChatColors(config.getString("CustomChatFormat"));
 
-		event.setFormat(customChatFormat.replaceAll("<player>", playerDisplayName).replaceAll("<message>",
-				convertChatColors(message)));
-	}
-
-	private String convertChatColors(String string) {
-		return MessageUtils.chat(string);
+		event.setFormat(MessageUtils.chat(config.getString("CustomChatFormat").replaceAll("<player>", playerDisplayName)
+				.replaceAll("<message>", message)));
 	}
 }
